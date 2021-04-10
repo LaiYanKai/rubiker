1f262c9e5a6a7906703ddf34245bb5130ee81cd4
LaiYanKai <lai.yankai@gmail.com>
2021-04-10 18:27:24 +0000

    2

---
+++ git.sh
@@ -0,0 +1,14 @@
+git config --global user.name "LaiYanKai"
+git config --global user.email "lai.yankai@gmail.com"
+##### initial steps ######
+
+#git init
+#git add .
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/LaiYanKai/rubiker.git
+#git push -u origin main
+
+git add .
+git commit -m $1
+git push
