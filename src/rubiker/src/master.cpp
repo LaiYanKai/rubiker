cc6b9bdfcdc586f73c482dd93a40b44382f2ffd1
LaiYanKai <laiyankai@gmail.com>
2021-04-11 06:22:56 +0000

    3

--- src/rubiker/src/master.cpp
+++ src/rubiker/src/master.cpp
@@ -3,26 +3,72 @@
 #include "std_msgs/Int32.h"
 
 #define THRESH 3
-int deg1 = 0;
-int deg2 = 0;
-int deg3 = 0;
-int deg4 = 0;
+#define WRISTCW 720
+#define WRISTACW -720
+#define HANDOPEN 540
+#define HANDCLOSE 720
+#define L_CW 1
+#define L_ACW 2
+#define L_CLOSE 3
+#define L_OPEN 4
+#define R_CW 5
+#define R_ACW 6
+#define R_CLOSE 7
+#define R_OPEN 8
+#define F_CW 9
+#define F_ACW 10
+#define F_CLOSE 11
+#define F_OPEN 12
+#define B_CW 13
+#define B_ACW 14
+#define B_CLOSE 15
+#define B_OPEN 16
+#define L_TURNOPEN 17
+#define R_TURNOPEN 18
+#define F_TURNOPEN 19
+#define B_TURNOPEN 20
+#define L_CW_R_ACW 21
 
-void cbDeg1(const std_msgs::Int32::ConstPtr& msg)
+int degLW = 0;
+int degRH = 0;
+int degLH = 0;
+int degRW = 0;
+int degFW = 0;
+int degBH = 0;
+int degFH = 0;
+int degBW = 0;
+
+void cbDegLW(const std_msgs::Int32::ConstPtr& msg)
 {
-   deg1 = msg->data;
+   degLW = msg->data;
 }
-void cbDeg2(const std_msgs::Int32::ConstPtr& msg)
+void cbDegRH(const std_msgs::Int32::ConstPtr& msg)
 {
-   deg2 = msg->data;
+   degRH = msg->data;
 }
-void cbDeg3(const std_msgs::Int32::ConstPtr& msg)
+void cbDegLH(const std_msgs::Int32::ConstPtr& msg)
 {
-   deg3 = msg->data;
+   degLH = msg->data;
 }
-void cbDeg4(const std_msgs::Int32::ConstPtr& msg)
+void cbDegRW(const std_msgs::Int32::ConstPtr& msg)
 {
-   deg4 = msg->data;
+   degRW = msg->data;
+}
+void cbDegFW(const std_msgs::Int32::ConstPtr& msg)
+{
+   degFW = msg->data;
+}
+void cbDegBH(const std_msgs::Int32::ConstPtr& msg)
+{
+   degBH = msg->data;
+}
+void cbDegFH(const std_msgs::Int32::ConstPtr& msg)
+{
+   degFH = msg->data;
+}
+void cbDegBW(const std_msgs::Int32::ConstPtr& msg)
+{
+   degBW = msg->data;
 }
 
 // W1C3
@@ -31,23 +77,140 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "master");
     ros::NodeHandle nm;
 
-    ros::Publisher pub_target1 = nm.advertise<std_msgs::Int32>("target1", 1, true);
-    ros::Publisher pub_target2 = nm.advertise<std_msgs::Int32>("target2", 1, true);
-    ros::Publisher pub_target3 = nm.advertise<std_msgs::Int32>("target3", 1, true);
-    ros::Publisher pub_target4 = nm.advertise<std_msgs::Int32>("target4", 1, true);
-    ros::Subscriber sub_deg1 = nm.subscribe("deg1", 1, cbDeg1);
-    ros::Subscriber sub_deg2 = nm.subscribe("deg2", 1, cbDeg2);
-    ros::Subscriber sub_deg3 = nm.subscribe("deg3", 1, cbDeg3);
-    ros::Subscriber sub_deg4 = nm.subscribe("deg4", 1, cbDeg4);
+    ros::Publisher pub_target1 = nm.advertise<std_msgs::Int32>("targetLW", 1, true);
+    ros::Publisher pub_target2 = nm.advertise<std_msgs::Int32>("targetRH", 1, true);
+    ros::Publisher pub_target3 = nm.advertise<std_msgs::Int32>("targetLH", 1, true);
+    ros::Publisher pub_target4 = nm.advertise<std_msgs::Int32>("targetRW", 1, true);
+    ros::Publisher pub_target1 = nm.advertise<std_msgs::Int32>("targetFW", 1, true);
+    ros::Publisher pub_target2 = nm.advertise<std_msgs::Int32>("targetBH", 1, true);
+    ros::Publisher pub_target3 = nm.advertise<std_msgs::Int32>("targetFH", 1, true);
+    ros::Publisher pub_target4 = nm.advertise<std_msgs::Int32>("targetBW", 1, true);
+    ros::Subscriber sub_deg1 = nm.subscribe("degLW", 1, cbDegLW);
+    ros::Subscriber sub_deg2 = nm.subscribe("degRH", 1, cbDegRH);
+    ros::Subscriber sub_deg3 = nm.subscribe("degLH", 1, cbDegLH);
+    ros::Subscriber sub_deg4 = nm.subscribe("degRW", 1, cbDegRW);
+    ros::Subscriber sub_deg1 = nm.subscribe("degFW", 1, cbDegFW);
+    ros::Subscriber sub_deg2 = nm.subscribe("degBH", 1, cbDegBH);
+    ros::Subscriber sub_deg3 = nm.subscribe("degFH", 1, cbDegFH);
+    ros::Subscriber sub_deg4 = nm.subscribe("degBW", 1, cbDegBW);
 
 
     ros::Rate looper(10);
 
     std_msgs::Int32 msg;
-    std::vector<int> targets1 = {-540}; // Wrist 
-    std::vector<int> targets2 = {720}; // Claw  1/6 {180, -180, 360, -360};
-    std::vector<int> targets3 = {720}; // Claw 1/6 {180, -180, 360, -360};
-    std::vector<int> targets4 = {-540}; // Wrist {180, -180, 360, -360};
+    bool upL = true, upR = true, upF = true, upB = true,
+         cL = true, cR = true, cF = true, cB = true, rotated = false;
+    std::vector<char>instructions = {'L','l','R','r','F','f','B','b','T','t','D','d'}
+    std::vector<int>states = {};
+    for(auto instruction: instructions)
+    {
+        if(instruction == 'L'){
+            if(rotated){
+                if(upF){
+                    if(!cF){
+                        states.push_back(F_CLOSE);
+                        cF = !cF;
+                    }
+                }else{
+                    if(!cF){
+                        states.push_back(F_CW);
+                        upF = !upF;
+                        states.push_back(F_CLOSE);
+                        cF = !cF;
+                    }else{
+                        states.push_back(F_TURNOPEN);
+                        cF = !cF;
+                        upF = !upF;
+                        states.push_back(F_CLOSE);
+                        cF = !cF;
+                    }
+                }
+                if(upB){
+                    if(!cB){
+                        states.push_back(B_CLOSE);
+                        cB = !cB;
+                    }
+                }else{
+                    if(!cB){
+                        states.push_back(B_CW);
+                        upB = !upB;
+                        states.push_back(B_CLOSE);
+                        cB = !cB;
+                    }else{
+                        states.push_back(B_TURNOPEN);
+                        cB = !cB;
+                        upB = !upB;
+                        states.push_back(B_CLOSE);
+                        cB = !cB;
+                    }
+                }if(!upR){
+                    if(!cR){
+                        states.push_back(R_CLOSE);
+                        cR = !cR;
+                    }
+                }else{
+                    if(!cR){
+                        states.push_back(R_CW);
+                        upR = !upR;
+                        states.push_back(R_CLOSE);
+                        cR = !cR;
+                    }else{
+                        states.push_back(R_TURNOPEN);
+                        cR = !cR;
+                        upR = !upR;
+                        states.push_back(R_CLOSE);
+                        cR = !cR;
+                }if(!upL){
+                    if(!cL){
+                        states.push_back(L_CLOSE);
+                        cL = !cL;
+                    }
+                }else{
+                    if(!cL){
+                        states.push_back(L_CW);
+                        upL = !upL;
+                        states.push_back(L_CLOSE);
+                        cL = !cL;
+                    }else{
+                        states.push_back(L_TURNOPEN);
+                        cL = !cL;
+                        upL = !upL;
+                        states.push_back(L_CLOSE);
+                        cL = !cL;
+                     }
+                  states.push_back(F_OPEN);
+                  cF = !cF;
+                  states.push_back(B_OPEN);
+                  cB = !cB;
+                  states.push_back(L_CW_R_ACW);
+                  upL = !upL;
+                  upR = !upR;
+                  xxx
+            }
+        } else if (instruction == 'l'){
+        } else if (instruction == 'R'){
+        } else if (instruction == 'r'){
+        } else if (instruction == 'F'){
+        } else if (instruction == 'f'){
+        } else if (instruction == 'B'){
+        } else if (instruction == 'b'){
+        } else if (instruction == 'T'){
+        } else if (instruction == 't'){
+        } else if (instruction == 'D'){
+        } else if (instruction == 'd'){
+        }
+    }
+
+    while(ros::ok())
+    {
+        looper.sleep();
+        ros::spinOnce();
+    }
+/*
+    std::vector<int> targets1 = {140}; // Wrist
+    std::vector<int> targets2 = {0}; // Claw  1/6 {180, -180, 360, -360};
+    std::vector<int> targets3 = {0}; // Claw 1/6 {180, -180, 360, -360};
+    std::vector<int> targets4 = {0}; // Wrist {180, -180, 360, -360};
 
     msg.data = targets1[0];
     pub_target1.publish(msg);
@@ -115,5 +278,5 @@ int main(int argc, char **argv) {
     }
 
 
-
+*/
 }
