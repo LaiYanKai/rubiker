24814851c3656dedbd1a3245d91c859a28836f63
LaiYanKai <lai.yankai@gmail.com>
2022-01-19 10:40:58 +0000

    220119

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ echo "BASH SOURCED"
 
 sudo ip addr add 10.0.0.1/24 dev eth0
 sudo ip link set dev eth0 up
+export ROS_MASTER_URI=http://10.0.0.1:11311
+export ROS_HOSTNAME=10.0.0.1
 
 # 		MTR 	K(BCM)	W(BCM)	Y(BCM) 	B(BCM) 	KP 	KI 	KD 	ErrThrs	LR
 export MotorLW="LW	15	14	23	18	1.5	0.001	2	5	40 1" #M5 #+ACW -CW
